c7b7f8b382755defe0e05cc7cecbff5be01cdc51
OskarGrzelak <oskargrzelak@gmail.com>
2018-08-08 13:14:37 +0200

    script.js update - word's checking algorithm

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const trigger = document.querySelector('.trigger');
 const lettersDisplay = document.querySelector('.letters-display');
 const roundsCounter = document.querySelector('#rounds-counter');
 const chceckLetterButton = document.querySelector('.btn--letter');
+const checkWordButton = document.querySelector('.btn--guess');
 const message = document.querySelector('.message');
 const checkedLetters = document.querySelector('.checked__letters');
 const inputs = document.querySelectorAll('input');
@@ -51,6 +52,7 @@ class Game {
 const getWordsLength = () => document.querySelector('#letters-number').value;
 const getRoundsNumber = () => document.querySelector('#rounds-number').value;
 const getLetter = () => document.querySelector('#guess-letter').value;
+const getWord = () => document.querySelector('#guess-word').value;
 
 const clearLettersDisplay = () => lettersDisplay.innerHTML = '';
 const clearInputs = () => inputs.forEach(el => el.value = '');
@@ -84,19 +86,43 @@ const displayLetter = letter => {
     });
 };
 
-const displayMessage = (state, letter) => {
-    if(state === 1) {
-        message.innerHTML = `<p class="message__text">Hidden word contains letter: <span class="message__letter">${letter}</span></p>`;
-        message.classList.remove('message--incorrect');
-        message.classList.add('message--correct');
-    } else if (state === -1) {
-        message.innerHTML = `<p class="message__text">Hidden word does not contain letter: <span class="message__letter">${letter}</span></p>`;
-        message.classList.remove('message--correct');
-        message.classList.add('message--incorrect');
-    } else {
-        message.innerHTML = `<p class="message__text">You have already checked letter: <span class="message__letter">${letter}</span></p>`;
-        message.classList.remove('message--correct');
-        message.classList.remove('message--incorrect');
+const displayCorrectWord = () => {
+    const arr = Array.from(document.querySelector('.letters-display').children);
+    arr.forEach((el, i) => el.innerHTML = game.hiddenWord[i]);
+};
+
+const displayMessage = (state, str) => {
+    switch(state) {
+        case 'correctLetter':
+            message.innerHTML = `<p class="message__text">Hidden word contains letter: <span class="message__letter">${str}</span></p>`;
+            message.classList.remove('message--incorrect');
+            message.classList.add('message--correct');
+            break;
+        case 'incorrectLetter':
+            message.innerHTML = `<p class="message__text">Hidden word does not contain letter: <span class="message__letter">${str}</span></p>`;
+            message.classList.remove('message--correct');
+            message.classList.add('message--incorrect');
+            break;
+        case 'checkedLetter':
+            message.innerHTML = `<p class="message__text">You have already checked letter: <span class="message__letter">${str}</span></p>`;
+            message.classList.remove('message--correct');
+            message.classList.remove('message--incorrect');
+            break;
+        case 'correctWord':
+            message.innerHTML = `<p class="message__text">You won! <span class="message__letter">${str}</span> was the hidden word</p>`;
+            message.classList.remove('message--incorrect');
+            message.classList.add('message--correct');
+            break;
+        case 'incorrectWord':
+            message.innerHTML = `<p class="message__text">Oh no! <span class="message__letter">${str}</span> was not the hidden word</p>`;
+            message.classList.remove('message--correct');
+            message.classList.add('message--incorrect');
+            break;
+        case 'wrongLength':
+            message.innerHTML = `<p class="message__text"><span class="message__letter">${str}</span> has a different length than the hidden word</p>`;
+            message.classList.remove('message--incorrect');
+            message.classList.remove('message--correct');
+            break;
     };
 };
 
@@ -138,7 +164,7 @@ startButton.addEventListener('click', e => {
 
 chceckLetterButton.addEventListener('click', e => {
     e.preventDefault();
-    let state = 0;
+    let state = 'checkedLetter';
     const letter = getLetter().toUpperCase();
     if(letter !== '') {
         clearInputs();
@@ -146,15 +172,38 @@ chceckLetterButton.addEventListener('click', e => {
             const isCorrect = checkLetter(letter);
             if(isCorrect) {
                 displayLetter(letter);
-                state = 1;
+                state = 'correctLetter';
             } else {
                 game.roundsNumber -= 1;
                 displayRoundsCounter(game.roundsNumber);
-                state = -1;
+                state = 'incorrectLetter';
             };
             displayCheckedLetters(letter);
             game.checked.push(letter);
         };
         displayMessage(state, letter);
+    };
+});
+
+checkWordButton.addEventListener('click', e => {
+    e.preventDefault();
+    let state = 'wrongLength';
+    const word = getWord().toUpperCase();
+    if(word !=='') {
+        clearInputs();
+        if(word.length === game.hiddenWord.length) {
+            if(word === game.hiddenWord) {
+                displayCorrectWord();
+                menu.classList.remove('menu__hidden');
+                trigger.style.marginBottom = '2rem';
+                triggerButton.style.transform = 'rotate(0)';
+                state = 'correctWord';
+            } else {
+                game.roundsNumber -= 1;
+                displayRoundsCounter(game.roundsNumber);
+                state = 'incorrectWord';
+            };
+        };
+        displayMessage(state, word);
     };
 });
